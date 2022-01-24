96dd101bf0f38964ed797d860448aeda2674f2f7
Will Greenberg <greenberg.will@gmail.com>
2022-01-24 09:37:33 -0800

    add ListOfPlaces function to scripts
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// Logic for ListOfPlaces
+
+function ListOfPlaces() {
+  this.place = {}
+}
+
+// Logic for Place
+
 function Place(location,landmark,timeOfYear){
   this.location=location;
   this.landmark=landmark;
